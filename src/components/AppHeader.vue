171857fdb4f8eefb2b6133eb02356b2d43eda98a
RiccardoVoltolina <riky.volto@live.com>
2023-12-24 20:10:45 +0100

    modify navbar layout

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -14,15 +14,18 @@ export default {
 <template>
   <header>
     <nav class="navbar navbar-expand-sm navbar-light bg-primary">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="/">Home</a>
+      <div class="container-fluid mx-5">
+        <a class="navbar-brand" href="/">Logo</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarID"
           aria-controls="navbarID" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarID">
-          <div class="navbar-nav">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <div class="navbar-nav d-flex justify-content-end w-100">
+            <a class="nav-link active" aria-current="page" href="#">Mare</a>
+            <a class="nav-link active" aria-current="page" href="#">Montagna</a>
+            <a class="nav-link active" aria-current="page" href="#">Subaquea</a>
+
 
           </div>
         </div>
